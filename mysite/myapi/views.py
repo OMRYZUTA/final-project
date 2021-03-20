8d1cbe5ef1e9ab9ab97504dd7862bfe61c770510
OMRYZUTA <omrizu@mta.ac.il>
2021-03-20 18:53:07 +0200

    add user id

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
 from .serializers import PositionSerializer
 from .models import Position
+
+
+class PositionList(generics.ListAPIView):
+    serializer_class = PositionSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Position.objects.filter(user_id=user)
+
 
 class PositionViewSet(viewsets.ModelViewSet):
     queryset = Position.objects.all().order_by('job_title')

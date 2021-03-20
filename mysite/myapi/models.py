812248ed5d32426ee00a8cc24ea34664386d23d8
Orna Lipkin <fifilp@gmail.com>
2021-03-20 20:31:05 +0200

    filter positions according to user ID

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 class Position(models.Model):
+    user_id = models.IntegerField(null=True)
+    job_title = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)    
+    country = models.CharField(max_length=100, default = 'Israel')
+    city = models.CharField(max_length=100)
+
+    #ForeignKey(UserSocialAuth.uid, on_delete=models.CASCADE)
+
+    # email_address = forms.EmailField() #delete later
+
+    def __str__(self):
+        return self.job_title #update later
+
+class ApplicationProcess(models.Model):
     job_title = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
     user_id = models.IntegerField(null=True)
@@ -9,4 +23,4 @@ class Position(models.Model):
     # email_address = forms.EmailField() #delete later
 
     def __str__(self):
-        return self.job_title  # yet to be defined, delete later
+        return self.job_title #update later
--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
-
 from rest_framework import viewsets, generics
-
 from .serializers import PositionSerializer
 from .models import Position
+from django.contrib.auth.decorators import login_required
 
 
-class PositionList(generics.ListAPIView):
-    serializer_class = PositionSerializer
+# class PositionList(generics.ListAPIView):
+#     serializer_class = PositionSerializer
 
-    def get_queryset(self):
-        user = self.request.user
-        return Position.objects.filter(user_id=user)
+#     def get_queryset(self):
+#         user = self.request.user
+#         return Position.objects.filter(user_id=user)
 
 
 class PositionViewSet(viewsets.ModelViewSet):
-    queryset = Position.objects.all().order_by('job_title')
+
     serializer_class = PositionSerializer
+    queryset = Position.objects.all()
+
+    @login_required
+    def get_queryset(self):
+        queryset = self.queryset
+        query_set = queryset.filter(user_id=self.request.user.id)
+        return query_set

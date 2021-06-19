b89bb9c5d7bd906ffc3e194e30d8d215bac9093c
OMRYZUTA <omrizu@mta.ac.il>
2021-06-19 18:25:24 +0300

    initial homepage

--- frontend/src/pages/Home/EnhancedTable.js
+++ frontend/src/pages/Home/EnhancedTable.js
@@ -21,24 +21,24 @@ import Switch from '@material-ui/core/Switch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs) {
+    return { name, calories, fat, carbs };
 }
 
 const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
+    createData('Cupcake', 305, 3.7, 67),
+    createData('Donut', 452, 25.0, 51),
+    createData('Eclair', 262, 16.0, 24),
+    createData('Frozen yoghurt', 159, 6.0, 24),
+    createData('Gingerbread', 356, 16.0, 49),
+    createData('Honeycomb', 408, 3.2, 87),
+    createData('Ice cream sandwich', 237, 9.0, 37),
+    createData('Jelly Bean', 375, 0.0, 94),
+    createData('KitKat', 518, 26.0, 65),
+    createData('Lollipop', 392, 0.2, 98),
+    createData('Marshmallow', 318, 0, 81),
+    createData('Nougat', 360, 19.0, 90),
+    createData('Oreo', 437, 18.0, 63),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -68,11 +68,10 @@ function stableSort(array, comparator) {
 }
 
 const headCells = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+    { id: 'company_name', numeric: false, disablePadding: true, label: 'Company' },
+    { id: 'job_title', numeric: false, disablePadding: true, label: 'Position' },
+    { id: 'status', numeric: false, disablePadding: true, label: 'Status' },
+    { id: 'date', numeric: true, disablePadding: true, label: 'Date' },// delete later change to next stage
 ];
 
 function EnhancedTableHead(props) {
@@ -85,17 +84,11 @@ function EnhancedTableHead(props) {
         <TableHead>
             <TableRow>
                 <TableCell padding="checkbox">
-                    <Checkbox
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{ 'aria-label': 'select all desserts' }}
-                    />
                 </TableCell>
                 {headCells.map((headCell) => (
                     <TableCell
                         key={headCell.id}
-                        align={headCell.numeric ? 'right' : 'left'}
+                        align='left'
                         padding={headCell.disablePadding ? 'none' : 'default'}
                         sortDirection={orderBy === headCell.id ? order : false}
                     >
@@ -120,9 +113,7 @@ function EnhancedTableHead(props) {
 
 EnhancedTableHead.propTypes = {
     classes: PropTypes.object.isRequired,
-    numSelected: PropTypes.number.isRequired,
     onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
     order: PropTypes.oneOf(['asc', 'desc']).isRequired,
     orderBy: PropTypes.string.isRequired,
     rowCount: PropTypes.number.isRequired,
@@ -150,37 +141,21 @@ const useToolbarStyles = makeStyles((theme) => ({
 
 const EnhancedTableToolbar = (props) => {
     const classes = useToolbarStyles();
-    const { numSelected } = props;
 
     return (
         <Toolbar
-            className={clsx(classes.root, {
-                [classes.highlight]: numSelected > 0,
-            })}
+            className={clsx(classes.root)}
         >
-            {numSelected > 0 ? (
-                <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
-                    {numSelected} selected
-                </Typography>
-            ) : (
-                <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-                    Application Process
-                </Typography>
-            )}
+            <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+                Application Process
+            </Typography>
 
-            {numSelected > 0 ? (
-                <Tooltip title="Delete">
-                    <IconButton aria-label="delete">
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            ) : (
-                <Tooltip title="Filter list">
-                    <IconButton aria-label="filter list">
-                        <FilterListIcon />
-                    </IconButton>
-                </Tooltip>
-            )}
+            <Tooltip title="Filter list">
+                <IconButton aria-label="filter list">
+                    <FilterListIcon />
+                </IconButton>
+            </Tooltip>
+
         </Toolbar>
     );
 };
@@ -300,25 +275,18 @@ export default function EnhancedTable() {
                                         <TableRow
                                             hover
                                             onClick={(event) => handleClick(event, row.name)}
-                                            role="checkbox"
-                                            aria-checked={isItemSelected}
                                             tabIndex={-1}
                                             key={row.name}
-                                            selected={isItemSelected}
                                         >
                                             <TableCell padding="checkbox">
-                                                <Checkbox
-                                                    checked={isItemSelected}
-                                                    inputProps={{ 'aria-labelledby': labelId }}
-                                                />
                                             </TableCell>
                                             <TableCell component="th" id={labelId} scope="row" padding="none">
                                                 {row.name}
                                             </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
-                                            <TableCell align="right">{row.carbs}</TableCell>
-                                            <TableCell align="right">{row.protein}</TableCell>
+                                            <TableCell align="left">{row.calories}</TableCell>
+                                            <TableCell align="left">{row.fat}</TableCell>
+                                            <TableCell align="left">{row.carbs}</TableCell>
+
                                         </TableRow>
                                     );
                                 })}

784fb1f5367e77660ea9b45c8fad137bd8949769
OMRYZUTA <omrizu@mta.ac.il>
2021-08-12 19:49:54 +0300

    background yellow

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -6,36 +6,28 @@ import { spacing } from '@material-ui/system';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+		
 		border: 0,
 		borderRadius: 3,
 		boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
 		color: 'white',
 		height: 48,
 		padding: '0 30px',
+		backgroundColor:'#ffffbf'
 	},
-	container: {
-		width: '95%',
-		spacing: '0',
-		// direction: 'column',
-		// alignItems: 'center',
-		// justify: 'center',
-		// alignContent: 'center',
-		// 	margin- left: auto,
-		// // margin- right: auto,
-	},
+
 }))
 
 const Index = () => {
 	const classes = useStyles();
 
 	return (
-		<Grid container spacing={0} justify="center" alignItems="center" style={{ minHeight: "100vh" }} >
+		<Grid className={classes.root} container spacing={0} justify="center" alignItems="center" style={{ minHeight: "100vh" }} >
 			<Grid item style={{ width: "95%" }} >
 				<EnhancedTable>
 				</EnhancedTable>
 			</Grid>
-		</Grid >
+		</Grid>
 	)
 }
 

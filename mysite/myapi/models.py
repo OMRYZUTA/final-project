ac0fc2e014f8f16fcfd17ee488ab8f92fb8ce960
Orna Lipkin <fifilp@gmail.com>
2021-08-15 15:10:42 +0300

    delete on cascade working well with onetoone field position

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -63,16 +63,23 @@ class Position(models.Model):
     company_name = models.CharField(max_length=100, null=True, blank=True)
     country = models.ForeignKey(
         Countries, null=False, default="IL", on_delete=models.DO_NOTHING)
-    # later update city field with Local-Flavor library?
     city = models.CharField(max_length=100, null=True, blank=True)
-    # EmailField(max_length=None, min_length=None, allow_blank=False)
     initial_contact_email_address = models.EmailField(
         max_length=100, null=True, blank=True)
-    # URLField(max_length=200, min_length=None, allow_blank=False)
     job_posting_URL = models.URLField(max_length=250, null=True, blank=True)
     # maybe change to textField?
     about_the_job = models.CharField(
         max_length=500, null=True, blank=True)  # roughly 2 paragraphs
+
+    #     class Restaurant(models.Model):
+    # place = models.OneToOneField(
+    #     Place,
+    #     on_delete=models.CASCADE,
+    #     primary_key=True,
+    # )
+
+    application_process_id = models.OneToOneField(
+        'ApplicationProcess', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.job_title  # update later
@@ -98,17 +105,14 @@ class Stage(models.Model):
 class ApplicationProcess(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.IntegerField(null=True)
-    # reference_set
-    # what about contact_set?
     note = models.TextField(null=True, blank=True)
+    reference = models.TextField(null=True, blank=True)
     last_modified = models.DateField(auto_now_add=True, null=True, blank=True)
     # sent_resume= models.FileField()
-    # will be one to many - several different application process will have same position (after parsing from other APIs)
-    position = models.OneToOneField(
-        Position, on_delete=models.CASCADE, null=True, blank=True)
-# need to change null = False delete later
     status = models.ForeignKey(
-        'Status', null=True, default="IN", on_delete=models.DO_NOTHING)
+        'Status', null=False, default="IN", on_delete=models.DO_NOTHING)
+    # position = models.OneToOneField(
+    #     Position, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.position.job_title  # update later
+        return self.id

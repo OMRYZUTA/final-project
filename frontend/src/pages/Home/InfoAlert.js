2e54b019f8dce40f19d81b9755dc39d3daf9f610
OMRYZUTA <omrizu@mta.ac.il>
2021-09-10 13:50:39 +0300

    added gray color to info alert

--- frontend/src/pages/Home/InfoAlert.js
+++ frontend/src/pages/Home/InfoAlert.js
@@ -8,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     step: {
         backgroundColor: '#FFFFC5',//yellow
+        color: '#5f676e',// gray
     },
 
 }));

3e4d7e236653c2610a15b89683793924004b0407
OMRYZUTA <omrizu@mta.ac.il>
2021-10-02 19:43:33 +0300

    files urls work

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -61,6 +61,7 @@ class Position(models.Model):
     def __str__(self):
         return str(self.job_title)
 
+
 class Stage(models.Model):
     id = models.AutoField(primary_key=True)
     application_process_id = models.ForeignKey(
@@ -74,6 +75,7 @@ class Stage(models.Model):
 
     def __str__(self):
         return str(self.stage_date)
+
 
 class ApplicationProcess(models.Model):
     id = models.AutoField(primary_key=True)
@@ -128,7 +130,7 @@ class Document(models.Model):
         ApplicationProcess, on_delete=models.SET_NULL, null=True)
     uploaded_at = models.DateField(auto_now_add=True, null=True, blank=True)
     file = models.FileField(upload_to='uploads/', null=True, blank=True)
-    file_url = models.URLField(null=True, blank=True)
+    file_url = models.CharField(max_length=2000, null=True, blank=True)
 
     def __str__(self):
         return str(self.user_id)
